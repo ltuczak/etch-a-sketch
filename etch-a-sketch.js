e2ea5a90fa7a20b8ce07d9895c3855a33dd1a68e
ltuczak <logan.tucz@gmail.com>
2021-07-19 19:56:01 -0400

    Completion criteria met; one column is now grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -21,14 +21,22 @@ function removeColor() {
 };
 
 function makeGrid(length) {
-    for (let i = 0; i < length; i++) {
-        cells[i] = document.createElement('div');
-        cells[i].setAttribute('id',`${i+1}`);
-        cells[i].setAttribute('class','cell')
-        container.appendChild(cells[i]);
+    for (let j = 0; j < length; j++) {
+        rows[j] = document.createElement('div');
+        rows[j].setAttribute('id', `row${j+1}`);
+        rows[j].setAttribute('class', 'row');
+        container.appendChild(rows[j]);
+        for (let i = 0; i < length; i++) {
+            cells[i] = document.createElement('div');
+            cells[i].setAttribute('id',`cell${j+1}${i+1}`);
+            cells[i].setAttribute('class','cell')
+            rows[j].appendChild(cells[i]);
+        }
+        cells.forEach(cell => cell.addEventListener('mouseover',addColor));
     }
+    
 
-    cells.forEach(cell => cell.addEventListener('mouseover',addColor));
+    
 
     return
 }
@@ -46,11 +54,15 @@ function reset() {
         cells[i].remove();
     };
     
+    for (let j = 0; j < rows.length; j++) {
+        rows[j].remove();
+    }
+
     gridSize = prompt('Enter grid size length:')
     
     sideLength = Math.sqrt(gridSize);
 
-    if (gridSize > 100) {
+    if (gridSize > 144) {
         return alert('Error: max grid size exceeded');
     } else {
         makeGrid(Math.sqrt(gridSize));
